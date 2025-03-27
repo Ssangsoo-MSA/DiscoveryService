958fa4eccfed62dd2f6d293edc22a5ee37437dbd
SsangSoo <kss10080@naver.com>
2025-03-27 23:58:42 +0900

    project init

---
+++ src/test/java/com/ssangsoo/msa/eccomerce/DiscoveryServiceTests.java
@@ -0,0 +1,13 @@
+package com.ssangsoo.msa.eccomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
